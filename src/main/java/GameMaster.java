51a870853e48c1ef51c16c8de77db3e9001473c3
Cscott991 <scott.christian998@gmail.com>
2026-02-05 18:10:49 -0500

    Finished

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -1,3 +1,33 @@
 public class GameMaster {
+    private static int totalExperiencePoints;
+    public static void gainExperience(int xp){
+        totalExperiencePoints += xp;
+    }
+    //getter method
+    public static int getTeamXP(){
+        return totalExperiencePoints;
+    }
+    //method
+    public static void applyPowerUp(int xp){
+        totalExperiencePoints += xp;
+    }
+    //method
+    public static void applyPowerUp(double multiplier){
+        totalExperiencePoints = (int)(totalExperiencePoints * multiplier);
+    }
+    //method
+    public static void resetGame(){
+        totalExperiencePoints = 0;
+    }
 
+    public static void main(String[] args){
+        GameMaster quest = new GameMaster();
+        quest.gainExperience(500);
+        quest.applyPowerUp(100);
+        quest.applyPowerUp(2.5);
+        System.out.print(quest.getTeamXP());
+    }
 }
+
+
+
